225087e37bbeacaf1cf27849c692ed80d74d1e01
3h4x <ochach@gmail.com>
2017-05-25 13:12:08 +0200

    Removed not needed debug.log

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sed -i.bak -e "s/PASSWORD/$(uuidgen | sed 's/\-//g')/" -e "s/USERNAME/$(uuidgen | sed 's/-//g')/" /home/uasf/.bitcoin/bitcoin.conf
-$BITCOIN_DIR/bin/bitcoind
-tail -F ~/.bitcoin/debug.log
+$BITCOIN_DIR/bin/bitcoind -listen -nodaemon -printtoconsole
